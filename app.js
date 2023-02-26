7aea0554f61284da26f004f14a91cacae5f723cc
Nahin27 <nahin271199@gmail.com>
2023-02-26 16:51:48 +0600

    Added the count page

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 import { serve } from "./deps.js";
+import { configure, renderFile } from "./deps.js";
 
-const handleRequest = (request) => {
-    return new Response("Hello World!");
+configure({
+    views: `${Deno.cwd()}/views/`,
+});
+
+const responseDetails = {
+    headers: {
+        "Content-Type": "text/html;charset=UTF-8",
+    },
+};
+
+const data = {
+    count: 0,
+};
+
+const handleRequest = async (request) => {
+    const url = new URL(request.url);
+    const path = url.pathname;
+    if(path === "/count") {
+        data.count++;
+        return new Response(await renderFile("count.eta", data), responseDetails);
+    }
+
+    return new Response("Hello you!");
 }
 
 serve(handleRequest, { port: 7777 });
