faeb23128de53a083331ec3cc3dd191efd87238f
Nahin27 <nahin271199@gmail.com>
2023-03-03 11:17:07 +0600

    counting rows in database

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 import { serve } from "./deps.js";
 import { configure, renderFile } from "./deps.js";
+import { sql } from "./database/database.js";
 
 configure({
-    views: `${Deno.cwd()}/views/`,
+  views: `${Deno.cwd()}/views/`,
 });
 
 const responseDetails = {
-    headers: {
-        "Content-Type": "text/html;charset=UTF-8",
-    },
+  headers: { "Content-Type": "text/html;charset=UTF-8" },
 };
 
 const data = {
-    count: 0,
+  count: 0,
 };
 
 const handleRequest = async (request) => {
-    const url = new URL(request.url);
-    const path = url.pathname;
-    if(path === "/count") {
-        data.count++;
-        return new Response(await renderFile("count.eta", data), responseDetails);
+  const url = new URL(request.url);
+  if (url.pathname === "/count") {
+    data.count++;
+    return new Response(await renderFile("count.eta", data), responseDetails);
+  }
+
+  if (url.pathname === "/addresses") {
+    const rows = await sql`SELECT COUNT(*) as count FROM addresses`;
+    let rowCount = -42;
+    if (rows.length > 0) {
+      rowCount = rows[0].count;
     }
 
-    return new Response("Hello you!");
-}
+    return new Response(`Total rows: ${rowCount}`);
+  }
+
+  return new Response("Hello you!");
+};
 
 serve(handleRequest, { port: 7777 });
